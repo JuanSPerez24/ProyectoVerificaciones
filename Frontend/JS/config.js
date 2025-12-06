067fe9fd3b19edd91c261e1100d13d0b723b8f94
JuanSPerez24 <sebastianperez11321@gmail.com>
2025-12-06 18:57:27 -0500

    ConfigiracionConfig para rutas de api

--- Frontend/JS/config.js
+++ Frontend/JS/config.js
@@ -1,6 +1,6 @@
 const API_URL = 
   window.location.hostname === 'localhost' 
-    ? 'http://localhost:3000'
-    : `${window.location.origin}`;
+    ? 'http://localhost:3000/api'
+    : `${window.location.origin}/api`;
 
 export default API_URL;
