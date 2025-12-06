4c9dad77b2c77d31595c8f8e338b54d53d28823d
JuanSPerez24 <sebastianperez11321@gmail.com>
2025-12-06 02:09:16 -0500

    configSever

--- Backend/server.js
+++ Backend/server.js
@@ -22,7 +22,7 @@ app.use(cors());
 app.use(express.static(path.join(__dirname, "../Frontend")));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, "../Frontend/index.html"));
+  res.sendFile(path.join(__dirname, "/index.html"));
 });
 
 app.get('/Buscar.html', (req, res) => {
