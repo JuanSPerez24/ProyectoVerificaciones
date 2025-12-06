50eb3f90f9e03fc6c8aaf23eba32f584935864f1
JuanSPerez24 <sebastianperez11321@gmail.com>
2025-12-06 03:04:31 -0500

    Prueba de configdb

--- Backend/db.js
+++ Backend/db.js
@@ -4,15 +4,20 @@ import mysql from "mysql2";
 dotenv.config();
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER || 'root',
+  password: process.env.DB_PASSWORD || '',
+  database: process.env.DB_NAME || 'verificaciones'
 });
 
 db.connect(err => {
   if (err) {
     console.error("Error en la conexión con MySQL:", err);
+    console.error("Usando variables:", {
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      database: process.env.DB_NAME
+    });
     return;
   }
   console.log("Conexión exitosa con MySQL");
