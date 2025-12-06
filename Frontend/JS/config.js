56739f7c8698be2b52e5b86647f781503ce44ab3
JuanSPerez24 <sebastianperez11321@gmail.com>
2025-12-06 02:25:56 -0500

    ExporApiUrl

--- Frontend/JS/config.js
+++ Frontend/JS/config.js
@@ -2,3 +2,5 @@ const API_URL =
   window.location.hostname === 'localhost' 
     ? 'http://localhost:3000/api'
     : 'https://marcas-en-varificaciones-railway.up.railway.app/api';
+
+export default API_URL;
