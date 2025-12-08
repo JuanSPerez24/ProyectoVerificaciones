1197e1d2fda0fc8e50b8650c8b398b135f82750a
JuanSPerez24 <sebastianperez11321@gmail.com>
2025-12-07 22:50:17 -0500

    Cierre sesion despues de 20 minutos

--- Frontend/JS/CerrarSesion.js
+++ Frontend/JS/CerrarSesion.js
@@ -1,16 +1,24 @@
-
 const BotonCerrarSesion = document.getElementById("ButtonCerrarSesion");
 
 function CerrarSesion() {
+    // borrar datos de sesión
     localStorage.removeItem("token");
     localStorage.removeItem("usuario");
+
+    // notificar a otras pestañas
+    localStorage.setItem("logout", Date.now());
+
     window.location.href = "/";
 }
 
-BotonCerrarSesion.addEventListener("click", () => {
-    CerrarSesion();
-});
+// botón manual
+if (BotonCerrarSesion) {
+    BotonCerrarSesion.addEventListener("click", () => {
+        CerrarSesion();
+    });
+}
 
+// si no hay token o usuario, redirigir
 function CierreSesionSinInicio() {
     const token = localStorage.getItem("token");
     const usuario = localStorage.getItem("usuario");
@@ -18,6 +26,37 @@ function CierreSesionSinInicio() {
     if (!token || !usuario) {
         window.location.href = "/";
     }
-};
+}
 
 CierreSesionSinInicio();
+
+// Sincrizacion entre pestañas
+window.addEventListener("storage", (event) => {
+    // si otra pestaña cerró sesión
+    if (event.key === "logout") {
+        window.location.href = "/";
+    }
+});
+
+let timeout;
+
+function ResetInactividad() {
+    // Reinicia el contador
+    clearTimeout(timeout);
+
+    // Programa el cierre dentro de 20 min
+    timeout = setTimeout(() => {
+
+        // Acción al expirar
+        CerrarSesion();
+        
+    }, 20 * 60 * 1000); // 20 minutos. Minutos, segundos, milisegundos
+}
+
+// Eventos que indican actividad
+document.addEventListener("mousemove", ResetInactividad);
+document.addEventListener("keydown", ResetInactividad);
+document.addEventListener("click", ResetInactividad);
+
+// Inicia el contador cuando carga la página
+ResetInactividad();
