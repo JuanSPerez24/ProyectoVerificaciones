b4abb5d9bd523f6d97c21b0913d3c55fb3f71f74
JuanSPerez24 <sebastianperez11321@gmail.com>
2025-12-07 19:00:42 -0500

    Creacion funcinon validacion ingreso usuario

--- Frontend/JS/CerrarSesion.js
+++ Frontend/JS/CerrarSesion.js
@@ -10,3 +10,14 @@ function CerrarSesion() {
 BotonCerrarSesion.addEventListener("click", () => {
     CerrarSesion();
 });
+
+function CierreSesionSinInicio() {
+    const token = localStorage.getItem("token");
+    const usuario = localStorage.getItem("usuario");
+    
+    if (!token || !usuario) {
+        window.location.href = "/";
+    }
+};
+
+CierreSesionSinInicio();
